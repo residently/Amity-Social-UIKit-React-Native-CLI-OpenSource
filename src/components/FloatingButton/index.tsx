7dda59fa3d0a4162d9eea9c9fe8262eab2af5162
Rob Aldred <rob@residently.com>
2025-02-03 14:59:22 +0000

    Floating button should take on primary color

--- src/components/FloatingButton/index.tsx
+++ src/components/FloatingButton/index.tsx
@@ -3,6 +3,8 @@ import { Pressable, View } from 'react-native';
 import { SvgXml } from 'react-native-svg';
 import { postIcon } from '../../svg/svg-xml-list';
 import { styles } from './styles';
+import { useTheme } from 'react-native-paper';
+import { MyMD3Theme } from '~/providers/amity-ui-kit-provider';
 
 interface IBackBtn {
   onPress: () => any;
@@ -12,13 +14,15 @@ export default function FloatingButton({
   onPress,
   isGlobalFeed = true,
 }: IBackBtn) {
+  const theme = useTheme() as MyMD3Theme;
+
   return (
     <View style={!isGlobalFeed ? styles.otherFeedContainer : styles.container}>
       <Pressable
         onPress={() => {
           onPress && onPress();
         }}
-        style={styles.button}
+        style={{ ...styles.button, backgroundColor: theme.colors.primary }}
       >
         <SvgXml xml={postIcon('#FFFFFF')} width="30" height="30" />
       </Pressable>
